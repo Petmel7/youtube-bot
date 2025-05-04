9740a6ded3091a72d10535bbfabf6510bfb4e410
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2025-05-04 13:56:37 +0300

    add

--- bot-frontend/src/config/languages.js
+++ bot-frontend/src/config/languages.js
@@ -2,7 +2,8 @@ const languages = [
     { code: "en", label: "English" },
     { code: "uk", label: "Українська" },
     { code: "de", label: "Deutsch" },
-    { code: "fr", label: "Français" }
+    { code: "fr", label: "Français" },
+    { code: "pl", label: "Polska" }
 ];
 
 export default languages;
